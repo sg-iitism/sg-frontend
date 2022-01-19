fee2f66c6b90410df61613fd27c1526183b7cdc0
Yash Vardhan <yashvardhan513@gmail.com>
2022-01-20 01:06:33 +0530

    implement club api to get all clubs

--- src/components/Clubs/index.tsx
+++ src/components/Clubs/index.tsx
@@ -1,51 +1,92 @@
-import { Row, Col } from "antd";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import { Row, Col, Space, Spin } from "antd";
 import { withTranslation } from "react-i18next";
-import Container from "../../common/Container";
 import ClubsContent from "../../content/ClubsPage.json";
-import { SvgIcon } from "../../common/SvgIcon";
 import "./styles.css";
 
 const ClubsComponent = () => {
-  return (
-    <div style={{marginTop: "4rem", marginBottom: "6rem"}}>
-      <h3 className="clubs_h3_science">Technical Clubs</h3>
-      <Row justify="space-between" id="tech_clubs">
-          {ClubsContent.science.map((club) => (
-              <Col lg={6} md={6} sm={12} xs={12}>
-                    <a href={club.club_url}>
-                        <div className="fests_div">
-                            <img src={club.club_logo} className="fests_img"></img>
-                            <div style={{marginTop: "2rem"}}>
-                            <span className="fests_name">{club.club_name}</span>
-                            </div>
-                            <div style={{marginTop: "0.3rem"}}>
-                            <span className="fests_detail">{club.club_description}</span>
-                            </div>
-                        </div>
-                    </a>
-              </Col>
-          ))}
-      </Row>
-      <h3 className="clubs_h3">Media And Cultural Clubs</h3>
-      <Row justify="space-between" id="cult_clubs">
-          {ClubsContent.media.map((club) => (
-              <Col lg={6} md={6} sm={12} xs={12}>
-                    <a href={club.club_url}>
-                        <div className="fests_div">
-                            <img src={club.club_logo} className="fests_img"></img>
-                            <div style={{marginTop: "2rem"}}>
-                            <span className="fests_name">{club.club_name}</span>
-                            </div>
-                            <div style={{marginTop: "0.3rem"}}>
-                            <span className="fests_detail">{club.club_description}</span>
-                            </div>
-                        </div>
-                    </a>
-              </Col>
-          ))}
-      </Row>
-    </div>
-  );
+    const [clubs, setClubs] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      const url = "https://sg-iitism-api.herokuapp.com/v1/clubs";
+  
+      const fetchData = async () => {
+        const result = await axios(url);
+  
+        console.log(result);
+        setClubs(result.data);
+        setLoading(false);
+      };
+  
+      fetchData();
+    }, []);
+
+    return (
+        <div style={{marginTop: "4rem", marginBottom: "6rem"}}>
+        <h3 className="clubs_h3_science">Technical Clubs</h3>
+        { !loading ?
+        <Row justify="space-between" id="tech_clubs">
+            {clubs.map((club) => {
+                if(club.division === "snt"){
+                    return (
+                        <Col lg={6} md={6} sm={12} xs={24} style={{marginBottom: "2rem"}}>
+                            <a href={"/clubs/" + club.id}>
+                                <div className="fests_div">
+                                    <img 
+                                      src={club.logoUrl ? club.logoUrl : "http://localhost:3000/img/icons/sg_logo.jpg"} 
+                                      className="fests_img">
+                                    </img>
+                                    <div style={{marginTop: "2rem"}}>
+                                    <span className="fests_name">{club.name}</span>
+                                    </div>
+                                    <div style={{marginTop: "0.3rem"}}>
+                                    <span className="fests_detail">{club.tagline}</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </Col>
+                    )
+                }
+            })}
+        </Row> : 
+            <div style={{textAlign: "center", marginBottom: "3rem"}}>
+              <Space size="middle" style={{textAlign: "center"}}><Spin size="large" /></Space>
+            </div>
+         }
+        <h3 className="clubs_h3">Media And Cultural Clubs</h3>
+        { !loading ?
+        <Row justify="space-between" id="cult_clubs">
+            {clubs.map((club) => {
+                if(club.division === "mnc"){
+                    return (
+                        <Col lg={6} md={6} sm={12} xs={24} style={{marginBottom: "2rem"}}>
+                            <a href={"/clubs/" + club.id}>
+                                <div className="fests_div">
+                                    <img 
+                                      src={club.logoUrl ? club.logoUrl : "http://localhost:3000/img/icons/sg_logo.jpg"} 
+                                      className="fests_img">
+                                    </img>
+                                    <div style={{marginTop: "2rem"}}>
+                                    <span className="fests_name">{club.name}</span>
+                                    </div>
+                                    <div style={{marginTop: "0.3rem"}}>
+                                    <span className="fests_detail">{club.tagline}</span>
+                                    </div>
+                                </div>
+                            </a>
+                        </Col>
+                    )
+                }
+            })}
+        </Row> :
+            <div style={{textAlign: "center", marginBottom: "3rem"}}>
+                <Space size="middle" style={{textAlign: "center"}}><Spin size="large" /></Space>
+            </div>
+        }
+        </div>
+    );
 };
 
 export default withTranslation()(ClubsComponent);
