c8921be7c5b86c686191ffbfc5897429a2799f46
Yash Vardhan <yashvardhan513@gmail.com>
2023-01-12 22:53:49 +0530

    shift api to sg-id

--- src/components/Announcements/index.js
+++ src/components/Announcements/index.js
@@ -45,7 +45,7 @@ const Announcements = () => {
   }
 
   useEffect(() => {
-    const url = "https://sg-api.onrender.com/v1/announcements";
+    const url = "https://sgiitism-api.onrender.com/v1/announcements";
 
     const fetchData = async () => {
       try {
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const NO_IMAGE_URL = '/img/no-image.jpg'
-export const BASE_URL = 'https://sg-api.onrender.com/v1/'
+export const BASE_URL = 'https://sgiitism-api.onrender.com/v1/'
